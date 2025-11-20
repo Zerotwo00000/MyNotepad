693c47ca7a1aa38a6ffe04200aaa526419b7d1f5
Zerotwo00000 <3293529428@qq.com>
2025-11-20 21:24:52 +0800

    第五次提交 处理新建文件、另存文件、保存文件、打开文件操作

--- mainwindow.h
+++ mainwindow.h
@@ -25,9 +25,24 @@ private slots:
 
     void on_actionReplace_triggered();
 
+    void on_actionNEW_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSaveAs_triggered();
+
+    void on_textEdit_textChanged();
+
 private:
     Ui::MainWindow *ui;
     QLabel statusCursorLabel;
     QLabel statusLabel;
+
+    QString filePath;
+    bool textChanged;
+
+    bool userEditConfirmed();
 };
 #endif // MAINWINDOW_H
