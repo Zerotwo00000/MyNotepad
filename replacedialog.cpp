60a0a981c31f25932c2362990c4dafc8d355b7d0
Zerotwo00000 <3293529428@qq.com>
2025-11-21 10:46:03 +0800

    第十次提交 处理替换窗口操作

--- replacedialog.cpp
+++ replacedialog.cpp
@@ -1,14 +1,102 @@
 #include "replacedialog.h"
 #include "ui_replacedialog.h"
+#include <QMessageBox>
 
-ReplaceDialog::ReplaceDialog(QWidget *parent)
+ReplaceDialog::ReplaceDialog(QWidget *parent , QPlainTextEdit * textEdit)
     : QDialog(parent)
     , ui(new Ui::ReplaceDialog)
 {
     ui->setupUi(this);
+    pTextEdit = textEdit;
+
+    ui->rbdown->setChecked(true);
 }
 
 ReplaceDialog::~ReplaceDialog()
 {
     delete ui;
 }
+
+void ReplaceDialog::on_btFindNext_clicked()
+{
+    QString target = ui->searchText->text();
+
+    if(target == "" || pTextEdit == nullptr)
+        return;
+
+    QString text = pTextEdit->toPlainText();
+    QTextCursor c = pTextEdit->textCursor();
+    int index = -1;
+
+    if(ui->rbdown->isChecked()){
+
+        index = text.indexOf(target,c.position(),ui->checkBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+        if(index >=0){
+            c.setPosition(index);
+            c.setPosition(index + target.length(),QTextCursor::KeepAnchor);
+
+            pTextEdit->setTextCursor(c);
+        }
+    }else if(ui->rbup->isChecked()){
+        index = text.lastIndexOf(target,c.position() - text.length() - 1,ui->checkBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+        if(index >=0){
+            c.setPosition(index + target.length());
+            c.setPosition(index,QTextCursor::KeepAnchor);
+
+            pTextEdit->setTextCursor(c);
+        }
+    }
+
+    if(index < 0){
+        QMessageBox msg(this);
+        msg.setWindowTitle("jishiben");
+        msg.setText(QString("找不到") + target);
+        msg.setWindowFlag(Qt::Drawer);
+        msg.setIcon(QMessageBox::Information);
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.exec();
+    }
+}
+
+
+void ReplaceDialog::on_replace_clicked()
+{
+    QString target = ui->searchText->text();
+    QString to = ui->replaceText->text();
+
+    if((pTextEdit != nullptr)&&(target != "")&&(to != "")){
+        QString selText = pTextEdit->textCursor().selectedText();
+
+        if(selText == target){
+            pTextEdit->insertPlainText(to);
+        }
+
+        on_btFindNext_clicked();
+    }
+
+}
+
+
+void ReplaceDialog::on_allreplace_clicked()
+{
+    QString target = ui->searchText->text();
+    QString to = ui->replaceText->text();
+
+    if((pTextEdit != nullptr)&&(target != "")&&(to != "")){
+        QString text = pTextEdit->toPlainText();
+
+        text.replace(target,to,ui->checkBox->isChecked()?Qt::CaseSensitive:Qt::CaseInsensitive);
+        pTextEdit->clear();
+
+        pTextEdit->insertPlainText(text);
+    }
+
+}
+
+
+void ReplaceDialog::on_btCancel_clicked()
+{
+    accept();
+
+}
+
